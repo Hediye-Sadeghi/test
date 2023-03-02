4d81d34e3e6bec5af55003ab41f80565b140765b
Hediye Sg <hedisg11@gmail.com>
2023-03-02 13:36:02 +0330

    update routes

--- routes.js
+++ routes.js
@@ -1,14 +1,20 @@
+
+
+
 let routes = [
-    { path: '/courses', element: < Courses /> },
+    { path: '/courses', element: <Courses /> },
     { path: '/course/:courseID', element: <MainCourse /> },
+    { path: '/news', element: <News /> },
+    { path: '/news/:newsID', element: <MainNews /> },
 
     {
         path: '/about/*', element: < About />, children: [
-            { path: "setting", element: < p style={{ textAlign: 'center' }}> Setting</p > },
-            { path: "dashboard", element: <p style={{ textAlign: 'center' }}>Dashboard</p> }
+            { path: "setting", element: <Setting/> },
+            { path: "dashboard", element: <Dashboard/> }
         ]
     },
     { path: '/login', element: <Login /> },
+    { path: '/register', element: <Register /> },
     { path: '/panel', element: <Panel /> },
     { path: '/dashboard', element: <Dashboard /> },
 ]
